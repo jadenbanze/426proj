c1792287987cd757fb93d4ffa68cb2199dcde427
Raaj Patel <41763998+Raajheer1@users.noreply.github.com>
2023-12-01 12:06:09 -0600

    Update thermal_auto.v

--- thermal_auto.v
+++ thermal_auto.v
@@ -1,52 +1,36 @@
 module TemporalThermalCovertChannel (
-    input wire clk,
-    input wire reset,
-    output reg [19:0] counter_output,
-    output reg [7:0] leds
+    input wire clk, // Clock input
+    output reg [7:0] leds // 8 LEDs output
 );
 
     reg [74:0] ring_oscillator;
     reg [19:0] counter;
     reg last_ring_bit; // Intermediate signal for ring_oscillator[74]
-    reg [31:0] second_counter; // Second counter for 60-second intervals
+    reg [31:0] display_timer; // Timer for controlling the LED display
 
     // The Ring Oscillator with 75 Inverters
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            ring_oscillator <= 75'b0;
-            last_ring_bit <= 0;
-            counter <= 20'b0;
-            second_counter <= 0;
-            counter_output <= 20'b0;
-        end else begin
-            ring_oscillator <= ~ring_oscillator;
-            last_ring_bit <= ring_oscillator[74];
-            
-            // Increment counter
-            if (last_ring_bit) begin
-                counter <= counter + 1'b1;
-            end
-            
-            // Manage 60-second intervals
-            second_counter <= second_counter + 1;
-            if (second_counter >= 12_000_000 * 60) begin // For a 12 MHz clock
-                counter_output <= counter;
-                second_counter <= 0;
-                counter <= 0; // Resetting the counter
-            end
+    always @(posedge clk) begin
+        ring_oscillator <= ~ring_oscillator;
+        last_ring_bit <= ring_oscillator[74];
+        
+        // Increment counter
+        if (last_ring_bit) begin
+            counter <= counter + 1'b1;
         end
+
+        // Update display timer
+        display_timer <= display_timer + 1;
     end
 
     // LED Display Logic
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            leds <= 8'b0;
-        end else if (second_counter >= 12_000_000 * 60 - 1) begin
-            // Display the counter value on LEDs for a short period before reset
-            leds <= counter_output[7:0]; // Display the lower 8 bits of the counter
-        end else begin
-            leds <= 8'b0;
-        end
+    always @(posedge clk) begin
+        case (display_timer[25:24]) // Using higher bits to divide the time
+            2'b00: leds <= 8'b0;                             // All LEDs off for the first 2 seconds
+            2'b01: leds <= counter[7:0];                     // Display first 8 bits of the counter
+            2'b10: leds <= counter[15:8];                    // Display next 8 bits of the counter
+            2'b11: leds <= {4'b0, counter[19:16]};           // Display last 4 bits (padded with zeros)
+            default: leds <= 8'b0;
+        endcase
     end
 
 endmodule
