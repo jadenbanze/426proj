915ad17a39e27049753937947509d91948688e4b
Raaj Patel <41763998+Raajheer1@users.noreply.github.com>
2023-12-01 12:26:18 -0600

    Update thermal_auto.v

--- thermal_auto.v
+++ thermal_auto.v
@@ -1,6 +1,6 @@
 module TemporalThermalCovertChannel (
     input wire clk, // Clock input
-    output reg [7:0] leds // 8 LEDs output
+    output led1, led2, led3, led4, led5, led6, led7, led8 // Individual LEDs
 );
 
     reg [74:0] ring_oscillator;
@@ -25,11 +25,11 @@ module TemporalThermalCovertChannel (
     // LED Display Logic
     always @(posedge clk) begin
         case (display_timer[25:24]) // Using higher bits to divide the time
-            2'b00: leds <= 8'b0;                             // All LEDs off for the first 2 seconds
-            2'b01: leds <= counter[7:0];                     // Display first 8 bits of the counter
-            2'b10: leds <= counter[15:8];                    // Display next 8 bits of the counter
-            2'b11: leds <= {4'b0, counter[19:16]};           // Display last 4 bits (padded with zeros)
-            default: leds <= 8'b0;
+            2'b00: {led1, led2, led3, led4, led5, led6, led7, led8} <= 8'b0;                            // All LEDs off for the first 2 seconds
+            2'b01: {led1, led2, led3, led4, led5, led6, led7, led8} <= counter[7:0];                    // Display first 8 bits of the counter
+            2'b10: {led1, led2, led3, led4, led5, led6, led7, led8} <= counter[15:8];                   // Display next 8 bits of the counter
+            2'b11: {led1, led2, led3, led4, led5, led6, led7, led8} <= {4'b0, counter[19:16]};          // Display last 4 bits (padded with zeros)
+            default: {led1, led2, led3, led4, led5, led6, led7, led8} <= 8'b0;
         endcase
     end
 
