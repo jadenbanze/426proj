cf844391a964314dc4b888f8ed5426732791463e
Raaj Patel <41763998+Raajheer1@users.noreply.github.com>
2023-12-03 14:37:50 -0600

    Update zybo.v

--- zybo.v
+++ zybo.v
@@ -4,39 +4,29 @@ module TemporalThermalCovertChannel (
     output reg led1, led2, led3, led4 // 4 LED outputs as reg
 );
 
-    (* keep = "true" *) reg [74:0] ring_oscillator; // Apply KEEP attribute to ring oscillator
-    reg [19:0] counter;
-    reg last_ring_bit; // Intermediate signal for ring_oscillator[74]
-    reg [31:0] display_timer; // Timer for controlling the LED display
+    reg [74:0] ring_oscillator; // Using a large number for more heat generation
 
-    // The Ring Oscillator with 75 Inverters
+    // The Ring Oscillator with Inverters in Series
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            // Reset all states and outputs
-            ring_oscillator <= 75'b0;
-            counter <= 0;
-            display_timer <= 0;
-            {led1, led2, led3, led4} <= 4'b0;
+            ring_oscillator <= {75{1'b1}}; // Initialize with all 1's
         end else begin
-            // Increment counter and update display timer
-            ring_oscillator <= ~ring_oscillator;
-            last_ring_bit <= ring_oscillator[74];
-            if (last_ring_bit) begin
-                counter <= counter + 1'b1;
-            end
-            display_timer <= display_timer + 1;
+            ring_oscillator[0] <= ~ring_oscillator[74];
+            ring_oscillator[74:1] <= ring_oscillator[73:0];
         end
     end
 
-    // LED Display Logic
     always @(posedge clk) begin
-        case (display_timer[23:22]) // Adjusting for a 100 MHz clock
-            2'b00: {led1, led2, led3, led4} <= 4'b0;                                 // All LEDs off for the first interval
-            2'b01: {led1, led2, led3, led4} <= counter[3:0];                         // Display first 4 bits of the counter
-            2'b10: {led1, led2, led3, led4} <= counter[7:4];                         // Display next 4 bits of the counter
-            2'b11: {led1, led2, led3, led4} <= counter[11:8];                        // Display the next 4 bits of the counter
-            default: {led1, led2, led3, led4} <= 4'b0111; // LEDs 1, 2, 3 off; LED 4 on
-        endcase
+        if (reset) begin
+            {led1, led2, led3} <= 3'b111; // Turn on LEDs 1, 2, and 3
+            led4 <= 1'b0;                 // Initialize LED 4
+        end else begin
+            {led1, led2, led3} <= 3'b111; // Keep LEDs 1, 2, and 3 constantly on
+            led4 <= ring_oscillator[0];   // Display the state of the ring oscillator on LED 4
+        end
     end
+
+    // Prevent optimization of the ring oscillator
+    (* DONT_TOUCH = "true" *) wire prevent_optimization = &ring_oscillator;
 
 endmodule
