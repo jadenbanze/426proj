32a154b14ae2b0a861180f492734159957ca4814
Raaj Patel <41763998+Raajheer1@users.noreply.github.com>
2023-12-03 13:29:04 -0600

    Update zybo.v

--- zybo.v
+++ zybo.v
@@ -1,11 +1,10 @@
 module TemporalThermalCovertChannel (
     input wire clk,      // Clock input
     input wire reset,    // Reset button
-    input wire enable,   // Enable button
     output reg led1, led2, led3, led4 // 4 LED outputs as reg
 );
 
-    reg [74:0] ring_oscillator;
+    (* keep = "true" *) reg [74:0] ring_oscillator; // Apply KEEP attribute to ring oscillator
     reg [19:0] counter;
     reg last_ring_bit; // Intermediate signal for ring_oscillator[74]
     reg [31:0] display_timer; // Timer for controlling the LED display
@@ -18,8 +17,8 @@ module TemporalThermalCovertChannel (
             counter <= 0;
             display_timer <= 0;
             {led1, led2, led3, led4} <= 4'b0;
-        end else if (enable) begin
-            // Increment counter and update display timer when enabled
+        end else begin
+            // Increment counter and update display timer
             ring_oscillator <= ~ring_oscillator;
             last_ring_bit <= ring_oscillator[74];
             if (last_ring_bit) begin
@@ -31,17 +30,13 @@ module TemporalThermalCovertChannel (
 
     // LED Display Logic
     always @(posedge clk) begin
-        if (enable) begin
-            case (display_timer[23:22]) // Using higher bits to divide the time
-                2'b00: {led1, led2, led3, led4} <= 4'b0;                                 // All LEDs off for the first interval
-                2'b01: {led1, led2, led3, led4} <= counter[3:0];                         // Display first 4 bits of the counter
-                2'b10: {led1, led2, led3, led4} <= counter[7:4];                         // Display next 4 bits of the counter
-                2'b11: {led1, led2, led3, led4} <= counter[11:8];                        // Display the next 4 bits of the counter
-                default: {led1, led2, led3, led4} <= 4'b0;
-            endcase
-        end else begin
-            {led1, led2, led3, led4} <= 4'b0; // Turn off LEDs if not enabled
-        end
+        case (display_timer[23:22]) // Adjusting for a 100 MHz clock
+            2'b00: {led1, led2, led3, led4} <= 4'b0;                                 // All LEDs off for the first interval
+            2'b01: {led1, led2, led3, led4} <= counter[3:0];                         // Display first 4 bits of the counter
+            2'b10: {led1, led2, led3, led4} <= counter[7:4];                         // Display next 4 bits of the counter
+            2'b11: {led1, led2, led3, led4} <= counter[11:8];                        // Display the next 4 bits of the counter
+            default: {led1, led2, led3, led4} <= 4'b0111; // LEDs 1, 2, 3 off; LED 4 on
+        endcase
     end
 
 endmodule
